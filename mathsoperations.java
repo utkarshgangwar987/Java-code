818a66872d6ff39b81609d6b706e38c244428f4a
utkarshgangwar987 <69318763+utkarshgangwar987@users.noreply.github.com>
2021-08-09 01:15:38 +0530

    input a value to find its square root

---
+++ mathsoperations.java
@@ -0,0 +1,72 @@
+import java.util.* ;
+ class mathsoperations {
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+        int x = sc.nextInt();
+       
+        System.out.println("your value is "  +Math.sqrt(x));
+
+
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+   
+
+    }
+}
