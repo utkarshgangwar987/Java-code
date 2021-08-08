99bdca3f53666ddf5e16cfbc1a295f4a6f05bdc5
utkarshgangwar987 <69318763+utkarshgangwar987@users.noreply.github.com>
2021-08-09 01:38:42 +0530

    Update mathsoperations.java

--- mathsoperations.java
+++ mathsoperations.java
@@ -1,10 +1,35 @@
+
 import java.util.* ;
  class mathsoperations {
     public static void main (String args[]){
         Scanner sc = new Scanner (System.in);
-        int x = sc.nextInt();
-       
+        float x = sc.nextFloat();
+       float y = sc.nextFloat();
         System.out.println("your value is "  +Math.sqrt(x));
+  System.out.println("your value is "  +Math.pow(x , y));
+    System.out.println("your value is "  +Math.cbrt(x));
+     System.out.println("your value is "  +Math.ceil(x));
+      System.out.println("your value is "  +Math.floor(x));
+       System.out.println("your value is "  +Math.round (x));
+        System.out.println("your value is "  +Math.abs (x));
+         System.out.println("your value is "  +Math.max(x , y));
+          System.out.println("your value is "  +Math.min(x , y));
+           System.out.println("your value is "  +Math.random()); 
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         
@@ -70,3 +95,65 @@ import java.util.* ;
 
     }
 }
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+   
+
+   
